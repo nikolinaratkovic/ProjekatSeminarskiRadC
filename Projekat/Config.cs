fb998c1efbcf2cf145ea72836be1c36914fadc24
nikolinaratkovic <NikolinaGIT2003>
2026-04-05 19:24:49 +0200

    prebacila sam da konfig ucitava u aplikacioni konfig fajl, pa svuda za bazu mozemo njega da koristimo

--- Projekat/Config.cs
+++ Projekat/Config.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace Projekat
 {
     public static class Config
     {
-        public const string ConnectionString =
-            "Server=DESKTOP-1U7QS82;Database=StudentPerformanceDB;Trusted_Connection=True;TrustServerCertificate=True;";
+        public static string ConnectionString =>
+            ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString
+            ?? throw new System.InvalidOperationException("Connection string 'DefaultConnection' not found in App.config.");
     }
 }
